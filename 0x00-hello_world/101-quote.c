89411b2928ed09e13c2e91d3901ee58de114f609
mohamed <mohammedabdelhamed725@gmail.com>
2023-08-14 14:28:11 -0700

    dsdsf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+/**
+ * main - entry point
+ *
+ * description: print a quote using write function
+ *
+ * Return: 1 (error).
+ */
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n";
+
+	write(1, quo, 59);
+	return (0);
+}
