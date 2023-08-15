f62ae8583dd0660d2810a69cd8dcf57fea535422
mohamed <mohammedabdelhamed725@gmail.com>
2023-08-15 13:11:51 -0700

    fref

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - using _putchar function to print from a to z
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
