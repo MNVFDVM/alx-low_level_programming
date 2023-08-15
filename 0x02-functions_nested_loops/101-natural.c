068c2863e3cd9a1ff98a19974700ad7e2cb7bc53
mohamed <mohammedabdelhamed725@gmail.com>
2023-08-15 15:57:55 -0700

    sfs

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int n, sum;
 
-	for (n = 0; n < 1024; n++)
+i	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3 == 0) || (n % 5 == 0))
 			sum += n;
 	}
-	printf("%d\n", n);
+	printf("%d\n", sum);
 	return (0);
 }
