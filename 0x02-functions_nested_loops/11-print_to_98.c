32ecdeffdd312a22860be24d573f64a0a265bdf9
mohamed <mohammedabdelhamed725@gmail.com>
2023-08-15 15:01:20 -0700

    sfs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,13 @@
  */
 void print_to_98(int n)
 {
-	int count;
+	int i;
 
 	if (n > 98)
 		for (i = n; i > 98; i--)
-			printf("%d, ", count);
+			printf("%d, ", i);
 	else
 		for (i = n; i < 98; i++)
-			printf("%d, ", count);
+			printf("%d, ", i);
 	printf("98\n");
 }
