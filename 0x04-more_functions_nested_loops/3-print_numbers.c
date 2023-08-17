e1186de0e2d3e22ce2b497a291913cce909aaa7c
mohamed <mohammedabdelhamed725@gmail.com>
2023-08-17 09:30:05 -0700

    ferf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar(n + 48);
+		n++;
+	} while (n >= 0 && n <= 9);
+	_putchar('\n');
+}
