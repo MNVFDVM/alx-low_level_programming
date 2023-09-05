992096140dcc0acb7cc15598d63e259672fddb46
mohamed <mohammedabdelhamed725@gmail.com>
2023-09-05 08:10:28 -0700

    dsf

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print the minimum number
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, j, r;
+	int c[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[i]);
+	r = 0;
+	if (n < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 0; j < 5 && n >= 0; j++)
+	{
+		while (n >= c[j])
+		{
+			r++;
+			n -= c[j];
+		}
+	}
+	printf("%d\n", r);
+	return (0);
+}
