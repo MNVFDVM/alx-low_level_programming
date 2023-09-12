9d34932946a80f837ee87bee8ab838321d039503
mohamed <mohammedabdelhamed725@gmail.com>
2023-09-12 10:12:14 -0700

    df

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ *
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: owners name
+ *
+ * description: long dog struuct in abig betty world
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
