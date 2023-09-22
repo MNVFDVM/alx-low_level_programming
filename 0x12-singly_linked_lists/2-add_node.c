c2effaa7be5065f50813c83ac49ff145163a42e1
MNVFDVM <mohammedabdelhamed725@gmail.com>
2023-09-22 14:14:22 -0700

    df

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,20 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *n;
+	list_t *new;
 	unsigned int l = 0;
 
 	while (str[l])
 		l++;
 
-	n = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (!n)
 		return (NULL);
 
-	n->str = strdup(str);
-	n->l = l;
-	n->next = (*head);
-	(*head) = n;
+	new->str = strdup(str);
+	new->l = l;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
