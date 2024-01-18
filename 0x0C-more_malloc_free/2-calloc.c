b9c3b56e3ca003915251d5083cfd4b32f8cd4b7d
mohamed <mohammedabdelhamed725@gmail.com>
2024-01-18 12:56:32 -0800

    sfdg

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *_memset - fill memory with constant byte
+ *
+ * @c: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ *
+ * Return: c
+ */
+char *_memset(char *c, char b, unsigned int n)
+{
+	char *ptr = c;
+
+	while (n--)
+		*c++ = b;
+	return (ptr);
+}
+/**
+ * _calloc - allocate memory for an array using malloc
+ *
+ * @nmemb: array length
+ * @size: size of each element
+ *
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *n;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	n = malloc(sizeof(int) * nmemb);
+	return (n);
+}
