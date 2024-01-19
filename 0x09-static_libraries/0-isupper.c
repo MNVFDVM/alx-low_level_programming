c2e84238328f868f81432eafaf6d66cd29f762e7
mohamed <mohammedabdelhamed725@gmail.com>
2024-01-19 09:51:12 -0800

    df

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @s: char to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int s)
+{
+	if (s >= 'A' && s <= 'Z')
+		return (1);
+	else
+		return (0);
+}
