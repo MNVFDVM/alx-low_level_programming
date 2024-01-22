9e8b259b406f6e388c12534e350f968b9b78a751
mohamed <mohammedabdelhamed725@gmail.com>
2024-01-22 14:46:40 -0800

    fdf

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,20 +13,15 @@ int main(int argc, char **argv)
 {
 	int i, sum = 0;
 
-	if (argc == 1)
-		printf("0\n");
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		if (isalpha(*argv[i]))
 		{
-			if (isalpha(*argv[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
