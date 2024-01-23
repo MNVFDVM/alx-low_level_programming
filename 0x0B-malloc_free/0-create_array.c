7a046c566f3c2509dc4e23f9fa6933e627240871
mohamed <mohammedabdelhamed725@gmail.com>
2024-01-23 13:55:54 -0800

    saa

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,8 @@ char *create_array(unsigned int size, char c)
 {
 	char *m = malloc(size);
 
-	if (size == 0 || m == 0)
-		return (0);
+	if (size == 0 || m == NULL)
+		return (NULL);
 	while (size--)
 		m[size] = c;
 	return (m);
