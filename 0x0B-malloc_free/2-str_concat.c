e61c4d676c3a324b35208509428838c0d315cd39
mohamed <mohammedabdelhamed725@gmail.com>
2024-01-23 18:28:21 -0800

    dsf

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,12 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char *x;
 	int i, j;
 
-	x = malloc(strlen(s1) + strlen(s2));
+	x = malloc(strlen(s1) + strlen(s2) + 1);
 	if (x == NULL || s1 == NULL || s2 == NULL)
 		return (NULL);
 	for (i = 0; s1[i]; i++)
 		x[i] = s1[i];
 	for (j = 0; s2[j]; j++, i++)
 		x[i] = s2[j];
+		x[i] = '\0';
 	return (x);
 }
