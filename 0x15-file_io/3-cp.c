6cde456bbdd9681e3c9ddeeb22fbc494c357a961
medo <mohammedabdelhamed725@gmail.com>
2024-02-12 15:53:22 -0800

    dg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,9 +9,7 @@
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
 #define READ_BUF_SIZE 1024
-
 
 /**
  * main - program
@@ -21,6 +19,7 @@
  *
  * Return: 1 success
  */
+
 int main(int ac, char **av)
 {
 	int from = 0, to = 0;
@@ -30,15 +29,13 @@ int main(int ac, char **av)
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 
-	from = open(av[1] O_RDONLY);
-
+	from = open(av[1], O_RDONLY);
 	if (from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | PERMISSIONS);
-
-		if (to == -1)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (to == -1)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
 	while ((p = read(from, buf, READ_BUF_SIZE)) > 0)
 		if (write(to, buf, p) != p)
@@ -47,13 +44,11 @@ int main(int ac, char **av)
 	if (p == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	from = close(from);
-	to = close(to);
-
-	if (from)
+	if (close(from) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from), exit(100);
 
-	if (to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from), exit(100);
+	if (close(to) == -1)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, to), exit(100);
+
 	return (EXIT_SUCCESS);
 }
