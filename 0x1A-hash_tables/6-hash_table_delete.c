c7b440ec00b8ba35bb8c89d680dd62ff4a35127e
medo <mohammedabdelhamed725@gmail.com>
2024-04-12 18:14:48 -0700

    tr

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,24 +8,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *bucket, *aux_free;
-	unsigned long int i = 0;
+	hash_node_t *b, *a;
+	unsigned long int j = 0;
 
 	if (!ht)
 		return;
 
-	for (i = 0; i < ht->size; i++)
+	for (j = 0; j < ht->size; j++)
 	{
-		bucket = ht->array[i];
-		while (bucket)
+		b = ht->array[j];
+		while (b)
 		{
-			aux_free = bucket;
-			bucket = bucket->next;
-			if (aux_free->key)
-				free(aux_free->key);
-			if (aux_free->value)
-				free(aux_free->value);
-			free(aux_free);
+			a = b;
+			b = b->next;
+			if (a->key)
+				free(a->key);
+			if (a->value)
+				free(a->value);
+			free(a);
 		}
 	}
 	free(ht->array);
