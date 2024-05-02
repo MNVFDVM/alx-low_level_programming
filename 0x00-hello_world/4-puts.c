3ea5fd83dece17289d0ba74866d60be9f56665d8
medo <mohammedabdelhamed725@gmail.com>
2024-05-02 12:08:52 -0700

    fd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - entry point
  *
- * return: 0 success
+ * Return: always 0 success
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
